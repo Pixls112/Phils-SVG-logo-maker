b830eedb8b89b3c96d0740d3a75338a434d6c0d7
Pixls112 <nguyen.phillip112@gmai.com>
2023-08-16 18:45:18 -0700

    added inquirer questions and function to test

---
+++ index.js
@@ -0,0 +1,35 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'insideText',
+        message: 'Please enter three characters for your logo.',
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color will your text be?',
+    },
+    {
+        type: 'list',
+        name: 'shapeChoice',
+        message: 'What logo shape would you like?',
+        choices : ['Circle', 'Square', 'Triangle']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'What color will your shape be?',
+    },
+];
+
+
+function init(){
+     return inquirer.prompt(questions).then((data) => {
+        console.log(data)
+     })
+}
+
+init();
